798081f9d9ebc3bed56c10425c54f6ad53fbcf69
Stephen Clibbery <sdclibbery@googlemail.com>
2017-11-05 00:27:53 +0000

    tidy up

--- LedDriver.ino/LedDriver.ino.ino
+++ LedDriver.ino/LedDriver.ino.ino
@@ -1,13 +1,9 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
-
 #include <Adafruit_NeoPixel.h>
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
+// Pin 5: GPIO 5: use D1 pin on lolin nodemcu, and connect ws2812 VIn to 3v...
 #define PIN            5
 
-// How many NeoPixels are attached to the Arduino?
+// How many NeoPixels are attached?
 #define NUMPIXELS      1
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
@@ -19,14 +15,14 @@ int delayval = 10;
 int frame = 0;
 
 void setup() {
-  pixels.begin(); // This initializes the NeoPixel library.
+  pixels.begin();
 }
 
 void loop() {
   for(int i=0;i<NUMPIXELS;i++){
     pixels.setPixelColor(i, pixels.Color(frame%31,(frame%31)/3,0));
   }
-  pixels.show(); // This sends the updated pixel color to the hardware.
-  delay(delayval); // Delay for a period of time (in milliseconds).
+  pixels.show();
+  delay(delayval);
   frame++;
 }
